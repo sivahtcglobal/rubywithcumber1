266bfa477a362f90d1725e4e5b595e6ee80da778
Ravishankar Tamilselvan <ravishankart.qa@gmail.com>
2018-01-05 14:02:44 +0530

    Intellify base gem Updates
    Intellify base gem Updates

---
+++ lib/intellify.rb
@@ -0,0 +1,14 @@
+require 'test-factory'
+require 'configatron'
+require 'json'
+require 'rest-client'
+require 'os'
+
+$: << File.dirname(__FILE__)+'/intellify'
+require 'intellify/intellify_base_page'
+Dir["#{File.dirname(__FILE__)}/intellify/*.rb"].each {|f| require f }
+Dir["#{File.dirname(__FILE__)}/intellify/pages/IntellifyEssentials/*.rb"].each {|f| require f }
+Dir["#{File.dirname(__FILE__)}/intellify/pages/**/*.rb"].each {|f| require f }
+
+
+
